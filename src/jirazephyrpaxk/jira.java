bf4ac339eb23dd5a714f146989b6c530d65d6de1
Anbarasi Ajithkumar <anbarasi@macrosoftindia.com>
2018-08-03 12:46:18 +0530

    Commit

---
+++ src/jirazephyrpaxk/jira.java
@@ -0,0 +1,30 @@
+package jirazephyrpaxk;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class jira {
+@Test
+public void zephyrtesting() throws InterruptedException
+	{
+		System.out.println(" Welcome to my world");
+		System.setProperty("webdriver.chrome.driver","F:\\selenium-files\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+driver.get("http://teammigrate:18080/#/user/login");
+driver.manage().window().maximize();
+String aTitle ="eDriver Client Application";
+String a=driver.getTitle();
+if (aTitle.contentEquals(a))
+{
+	System.out.println("Test failed");
+}
+	driver.findElement(By.id("userName")).sendKeys("hprasad");
+	driver.findElement(By.id("password")).sendKeys("Test@123");
+	driver.findElement(By.cssSelector(".btn.btn-login")).click();
+	Thread.sleep(3000);
+System.out.println("Test Passed");	
+}
+
+}
